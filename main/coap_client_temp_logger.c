8952813ffa6ddca59d9e9dcda4dd02417504c755
moop <mooped@gmail.com>
2018-02-03 22:35:29 +0000

    First pass implementation of communication with a CCS811 over I2C.

--- main/coap_client_temp_logger.c
+++ main/coap_client_temp_logger.c
@@ -11,6 +11,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#include "driver/i2c.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
@@ -221,6 +223,172 @@ static void wifi_conn_init(void)
     ESP_ERROR_CHECK( esp_wifi_start() );
 }
 
+#define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT   I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS  0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL    0x0         /*!< I2C ack value */
+#define NACK_VAL   0x1         /*!< I2C nack value */
+
+#define CCS811_ADDR 0x5a
+
+int co2_reading = -1;
+int voc_reading = -1;
+uint8_t raw_reading[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+void i2c_init()
+{
+  i2c_config_t conf;
+  conf.mode = I2C_MODE_MASTER;
+  conf.sda_io_num = 19;
+  conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+  conf.scl_io_num = 18;
+  conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
+  conf.master.clk_speed = 100000;
+  i2c_param_config(1, &conf);
+  i2c_driver_install(1, conf.mode, 0, 0, 0);
+}
+
+static esp_err_t i2c_poke(i2c_port_t i2c_num, uint8_t reg)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ( CCS811_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+static esp_err_t i2c_write(i2c_port_t i2c_num, uint8_t reg, uint8_t* data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ( CCS811_ADDR << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+static esp_err_t i2c_read(i2c_port_t i2c_num, uint8_t reg, uint8_t* data_rd, size_t size)
+{
+    esp_err_t poke_err = i2c_poke(i2c_num, reg);
+    if (poke_err != ESP_OK)
+    {
+      return poke_err;
+    }
+    if (size == 0) {
+        return ESP_OK;
+    }
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ( CCS811_ADDR << 1 ) | READ_BIT, ACK_CHECK_EN);
+    if (size > 1) {
+        i2c_master_read(cmd, data_rd, size - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, data_rd + size - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+static uint8_t ccs811_init()
+{
+  esp_err_t err;
+  i2c_init();
+
+  uint8_t status = 0x00;
+  err = i2c_read(1, 0x00, &status, 1);
+  ESP_LOGI(TAG, "Initial Status: [%x]", status);
+
+  uint8_t sw_reset[] = { 0x11, 0xe5, 0x72, 0x8a };
+  err = i2c_write(1, 0xff, sw_reset, 4);
+  if (err != ESP_OK)
+  {
+    ESP_LOGI(TAG, "Software Reset failed! [%x]", err);
+    return 0;
+  }
+
+  vTaskDelay(1000.f / 10.f);
+
+  err = i2c_poke(1, 0xf4);
+  if (err != ESP_OK)
+  {
+    ESP_LOGI(TAG, "App Start failed! [%x]", err);
+    return 0;
+  }
+
+  vTaskDelay(1000.f / 10.f);
+
+  status = 0x00;
+  err = i2c_read(1, 0x00, &status, 1);
+  if ((status & 0x90) == 0x90)
+  {
+    ESP_LOGI(TAG, "CCS811 Initialised. Status: [%x]", status);
+  }
+  else
+  {
+    ESP_LOGI(TAG, "Read Status failed! Status: [%x] Error: [%x]", status, err);
+    return 0;
+  }
+
+  uint8_t drive_mode = (0x01) << 4;
+  err = i2c_write(1, 0x01, &drive_mode, 1);
+  if (err != ESP_OK)
+  {
+    ESP_LOGI(TAG, "Set Drive Mode failed! Error: [%x]", err);
+    return 0;
+  }
+
+  return 1;
+}
+
+static void ccs811_sample(uint8_t* data/*[8]*/)
+{
+  esp_err_t err;
+
+  // Wait for a sample
+  uint8_t ready = 0;
+  while (!ready)
+  {
+    uint8_t status = 0x00;
+    err = i2c_read(1, 0x00, &status, 1);
+    if ((status & 0x90) == 0x90)
+    {
+      if (status & 0x08)
+      {
+        ESP_LOGI(TAG, "Data Ready. Status: [%x]", status);
+        ready = 1;
+      }
+      else
+      {
+        ESP_LOGI(TAG, "No data. Status: [%x]", status);
+        vTaskDelay(1000.f / 10.f);
+      }
+    }
+    else
+    {
+      ESP_LOGI(TAG, "Read Status failed! Status: [%x] Error: [%x]", status, err);
+      return;
+    }
+  }
+
+  // Read data
+  err = i2c_read(1, 0x02, data, 8);
+  if (err != ESP_OK )
+  {
+    ESP_LOGI(TAG, "Read Alg Result failed! Result: [CO2: 0x%x 0x%x VOC: 0x%x 0x%x Status: 0x%x Error: 0x%x Raw: 0x%x 0x%x] Error: %x", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], err);
+    return;
+  }
+
+  ESP_LOGI(TAG, "Read Alg Result: [CO2: 0x%x 0x%x VOC: 0x%x 0x%x Status: 0x%x Error: 0x%x Raw: 0x%x 0x%x]", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
+}
+
 void app_main(void)
 {
     ESP_ERROR_CHECK( nvs_flash_init() );
@@ -235,6 +403,20 @@ void app_main(void)
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
 
+    // Configure output pins
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = ((1 << 25) | (1 << 26) | (1 << 27));
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+
+    // Set /wak low, /int and /rst high
+    gpio_set_level(25, 0);
+    gpio_set_level(26, 1);
+    gpio_set_level(27, 1);
+
+    // Read temperature and humidity data from Si7007
     temp_high = 0;
     temp_low = 0;
     humidity_high = 0;
@@ -267,9 +449,25 @@ void app_main(void)
 
     ESP_LOGI(TAG, "Sampled...");
 
+    // Read CCS811 if present
+    ESP_LOGI(TAG, "Attempting to read CCS811...");
+
+    if (ccs811_init())
+    {
+      ccs811_sample(raw_reading);
+      co2_reading = ((uint16_t)(raw_reading[0]) << 8) + raw_reading[1];
+      voc_reading = ((uint16_t)(raw_reading[2]) << 8) + raw_reading[3];
+      ESP_LOGI(TAG, "CO2 PPM: %i VOC PPM: %i.", co2_reading, voc_reading);
+    }
+    else
+    {
+      ESP_LOGI(TAG, "No CCS811 present.");
+    }
+
     // Log data
     ESP_LOGI(TAG, "H:%d %d T:%d %d", humidity_high, humidity_low, temp_high, temp_low);
 
     // On with the show
     xTaskCreate(coap_example_task, "coap", 2048, NULL, 5, NULL);
 }
+
